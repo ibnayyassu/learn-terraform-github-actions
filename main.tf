c5786b85c57cfd439df59f613873a520b37827fa
Sergino Delia <153148978+SerginoDelia@users.noreply.github.com>
2025-01-07 23:32:58 -0500

    Update main.tf
    added updates

--- main.tf
+++ main.tf
@@ -38,7 +38,7 @@ data "aws_ami" "ubuntu" {
     name   = "name"
     values = ["al2023-ami-2023.*-x86_64"]
   }
-  owners = ["471112661949"] # Canonical
+  # owners = ["471112661949"] # Canonical
 }
 
 resource "aws_instance" "web" {
